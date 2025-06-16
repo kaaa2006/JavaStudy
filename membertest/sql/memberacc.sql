452e2465046016063e6b4192533cf71e433851bd
220-19\kaaa1877 <kiminseok0604@gmail.com>
2025-06-16 09:59:34 +0900

    6/16

--- membertest/sql/memberacc.sql
+++
@@ -1,5 +0,0 @@
-create user membertest identified by membertest 
-grant resource, connect to membertest 
-alter user membertest default tablespace users
-alter user membertest temporary tablespace temp 
-
